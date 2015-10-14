2e7b9676e7736dff3abfd018c4f59c034504d76d
David Bittle <dabi9710@colorado.edu>
2015-10-13 20:58:27 -0600

    fixed mscottsudo

--- puppetScript/sudoMichael.pp
+++ puppetScript/sudoMichael.pp
@@ -5,20 +5,5 @@ class { 'sudo':
 
 sudo::conf { 'mscott':
   priority => 10,
-  content  => "%mscott ALL=(ALL) /usr/sbin/shutdown 1[2-9][0-9]",
-}
-
-sudo::conf { 'mscott':
-  priority => 10,
-  content  => "%mscott ALL=(ALL) /usr/sbin/shutdown [2-9][2-9][0-9]",
-}
-
-sudo::conf { 'mscott':
-  priority => 10,
-  content  => "%mscott ALL=(ALL) /usr/sbin/shutdown [1-9][0-9][0-9][0-9]",
-}
-
-sudo::conf{ 'mscott':
-  priority => 10,
-  content  => "%mscott ALL=(ALL) /usr/sbin/shutdown -c",
+  content  => "%mscott ALL=(ALL) /usr/sbin/shutdown 1[2-9][0-9]","%mscott ALL=(ALL) /usr/sbin/shutdown [2-9][2-9][0-9]","%mscott ALL=(ALL) /usr/sbin/shutdown -c","%mscott ALL=(ALL) /usr/sbin/shutdown [1-9][0-9][0-9][0-9]",
 }
